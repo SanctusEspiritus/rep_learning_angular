8b13fcc1bb2c9b813f83c9636d66c5cea6bfa7fc
Akkerman <godgiven231998@gmail.com>
2022-02-05 00:21:20 +0300

    start project

---
+++ src/app/Card/card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    templateUrl: './card.component.html',
+    selector: 'app-card' 
+})
+
+export class CardComponent {}
---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { CardComponent } from './Card/card.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CardComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent, CardComponent]
+})
+export class AppModule { }
